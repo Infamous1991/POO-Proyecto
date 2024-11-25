16bbaee5e09a5419d0599a038710aa49dd63f49b
Infamosu1991 <mcadillov04@gmail.com>
2024-11-24 23:08:45 -0500

    half main screen

--- code/src/main/resources/static/js/index.js
+++ code/src/main/resources/static/js/index.js
@@ -3,8 +3,13 @@ function setInit(stat){
     const ds = document.getElementById("main");
 
     if (stat === true){
-        ds.classList.remove("Init")
-    } else{
         ds.classList.add("Init")
+    } else{
+        ds.classList.remove("Init")
     }
+}
+
+function toggleLogin(){
+    const target= document.getElementById("empleado");
+    target.classList.toggle("showLogin")
 }
