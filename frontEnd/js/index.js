7394945a464f90be432fd37f80970352d9792cd9
M <mcadillov04@gmail.com>
2024-12-02 10:53:45 -0500

    final

--- frontEnd/js/index.js
+++ frontEnd/js/index.js
@@ -13,3 +13,25 @@ function toggleLogin(){
     const target= document.getElementById("empleado");
     target.classList.toggle("showLogin")
 }
+
+function login(){
+    test= {
+        userName: $('#usnm').val(), 
+        password: $('#pssw').val()
+    }
+    console.log(test);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/login",
+        contentType: 'application/json',
+        data: JSON.stringify(test),
+        success: function (response) {
+            window.location.replace("empleado.html");
+            console.log("si")
+        },
+        error: function (response){
+            console.error("fail");
+        }
+        
+    });
+}
